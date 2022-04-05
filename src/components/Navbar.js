fe4e5d151619bed1262257f97de51f21188a9145
Matias Solis <solisma42@gmail.com>
2022-04-05 02:10:44 -0300

    completed html and css of the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import logo from '../assets/images/logo.svg'
+import cart from '../assets/icons/cart.svg'
+import down from '../assets/icons/down.svg'
 
 export class Navbar extends Component {
   render() {
     return (
-      <ul>
-        <li>WOMEN</li>
-        <li>MEN</li>
-        <li>KIDS</li>
-      </ul>
+      <nav className='navbar'>
+        <ul className='navbar__list'>
+          <li className='navbar__item'>WOMEN</li>
+          <li className='navbar__item'>MEN</li>
+          <li className='navbar__item'>KIDS</li>
+        </ul>
+        <img className='navbar__logo' src={logo} alt="logo" />
+        <ul className='navbar__list'>
+          <li className='navbar__item'>
+            <span className='navbar__currency'>$</span>
+            <img className='navbar__down' src={down} />
+          </li>
+          <li className='navbar__item'>
+            <img className='navbar__cart' src={cart} />
+          </li>
+        </ul>
+      </nav>
     )
   }
 }
