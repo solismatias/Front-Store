1ce1daccee9ef4efeb02c962853c05725607d953
Matias Solis <solisma42@gmail.com>
2022-04-04 23:12:35 -0300

    added base elements for the navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <ul>
+        <li>WOMEN</li>
+        <li>MEN</li>
+        <li>KIDS</li>
+      </ul>
+    )
+  }
+}
+
+export default Navbar
