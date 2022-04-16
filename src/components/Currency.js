d75415f7fd5c3a3ac45af6ca6148948117d7f220
Matias Solis <solisma42@gmail.com>
2022-04-16 16:55:46 -0300

    Dynamically displayed currencies

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-
+import { Query } from '@apollo/client/react/components';
+import { GET_CURRENCIES } from '../utils/queries';
 export class Currency extends Component {
   render() {
     return (
       <ul className='currency'>
-        <li className='currency__item'>$ USD</li>
-        <li className='currency__item'>€ EUR</li>
-        <li className='currency__item'>¥ JPY</li>
+        <Query query={GET_CURRENCIES}>
+          {({ data, loading, error }) => {
+            if (loading) return <span>Loading...</span>
+            const { currencies } = data
+            return currencies.map(currency => <li className='currency__item' key={currency.symbol}>{currency.symbol} {currency.label}</li>)
+          }}
+        </Query>
       </ul>
     )
   }
