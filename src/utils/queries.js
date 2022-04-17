43465e5c94c453ce3b08a0a1829eabc3da53318a
Matias Solis <solisma42@gmail.com>
2022-04-17 14:41:06 -0300

    Added names to queries, and added GET_PRODUCTS

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,26 +1,46 @@
 import { gql } from '@apollo/client';
 const GET_CATEGORIES = gql`
-  {
+  query getCategories {
     categories {
       name
-      products {
-        id
-        name
-        inStock
-        gallery
-        description
-        category
-      }
     }
   }
   `
 const GET_CURRENCIES = gql`
-  {
+  query getCurrencies{
     currencies {
       label
       symbol
     }
   }
+`
+const GET_PRODUCTS = gql`
+query getProducts($category: String!) {
+  category(input: { title: $category }) {
+    name
+    products {
+      id
+      name
+      gallery
+      attributes {
+        name
+        type
+        items {
+          displayValue
+          value
+        }
+      }
+      inStock
+      prices {
+        currency {
+          label
+          symbol
+        }
+        amount
+      }
+    }
+  }
+}
 `
 
 
